4746c30f08067f498ac62aaf7d4903d0bd2b333c
MaxCohcka <Cohcka@gmail.com>
2020-11-10 00:40:31 +0300

    v1

--- Server/src/main/java/Core.java
+++ Server/src/main/java/Core.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.ServerSocket;
 import java.nio.ByteBuffer;
 import java.nio.file.Path;
@@ -13,8 +14,8 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.codec.serialization.ObjectDecoder;
 
 public class Core {
-    private static String login = "login";
-    private static String password = "Pass";
+    private static String loginTmp = "login";
+    private static String passwordTmp = "Pass";
     private static final Path root = Paths.get("SrvStorage");
 
     Core(){
@@ -94,19 +95,51 @@ public class Core {
         }
     }
 
-    public void checkCommand(ByteBuffer command){
-        switch(command.[0]){
+    public String[] checkCommand(ByteBuf command) throws UnsupportedEncodingException {
+        byte[] tmp;
+        switch(command.readByte()){
             case Patterns.LOGINCODE:
-                int state = 1;
-                if(state == 1){
-
-                }
-            case Patterns.PASSWORDCODE:
+                int loginLen = command.readInt();
+                tmp = new byte[loginLen];
+                command.readBytes(tmp);
+                String login = new String(tmp, "UTF-8");
+                int passLen = command.readInt();
+                tmp = new byte[passLen];
+                command.readBytes(tmp);
+                String password = new String(tmp, "UTF-8");
             case Patterns.CHANGEDIR:
+                int pathCDLen = command.readInt();
+                tmp = new byte[pathCDLen];
+                command.readBytes(tmp);
+                String pathChange = new String(tmp, "UTF-8");
             case Patterns.DOWNLOADFILE:
+                int pathDWFLen = command.readInt();
+                tmp = new byte[pathDWFLen];
+                command.readBytes(tmp);
+                String pathDWF = new String(tmp, "UTF-8");
+                int fileDWFLen = command.readInt();
+                tmp = new byte[fileDWFLen];
+                command.readBytes(tmp);
+                String filenameDWF = new String(tmp, "UTF-8");
             case Patterns.UPLOADFILE:
+                int pathUPFLen = command.readInt();
+                tmp = new byte[pathUPFLen];
+                command.readBytes(tmp);
+                String pathUPF = new String(tmp, "UTF-8");
+                int fileUPLen = command.readInt();
+                tmp = new byte[fileUPLen];
+                command.readBytes(tmp);
+                String filenameUP = new String(tmp, "UTF-8");
+                //передача байтов самого файла
             case Patterns.DELETEFILE:
+                int pathDFLen = command.readInt();
+                tmp = new byte[pathDFLen];
+                command.readBytes(tmp);
+                String pathDF = new String(tmp, "UTF-8");
+                int fileDFLen = command.readInt();
+                tmp = new byte[fileDFLen];
+                command.readBytes(tmp);
+                String filenameDF = new String(tmp, "UTF-8");
         }
     }
-
 }
