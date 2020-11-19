3e2b442459dd0643582981dcbbba210c6e239e04
MaxCohcka <Cohcka@gmail.com>
2020-11-20 00:29:37 +0300

    Upload

--- Server/src/main/java/Core.java
+++ Server/src/main/java/Core.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.file.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Stream;
 
 import io.netty.bootstrap.ServerBootstrap;
@@ -92,9 +94,21 @@ public class Core {
 
     private static class MainHandler extends ChannelInboundHandlerAdapter {
         private String username;
+        private ExecutorService executorService;
+        private int state = 0;
+
+        byte[] tmp;
+        //File
+        private String fileName;
+        private Long fileSize;
+        private Long countPack;
+        private Long readPack;
+        private Long readBytes = 0l;
+
 
         public MainHandler(String username) {
             this.username = username;
+            executorService = Executors.newSingleThreadExecutor();
         }
 
         @Override
@@ -102,102 +116,119 @@ public class Core {
             checkCommand((ByteBuf) msg);
             //ctx.writeAndFlush((byte) 1); // ответное сообщение
         }
-    }
 
-    public static void checkCommand(ByteBuf command) throws UnsupportedEncodingException {
-        // test
-        while (command.isReadable()) {
-            System.out.print((char) command.readByte());
-        }
-        // test ^
-        byte[] tmp;
-        switch(command.readByte()) {
-            case Patterns.CHANGEDIR:
-                int pathCDLen = command.readInt();
-                tmp = new byte[pathCDLen];
-                command.readBytes(tmp);
-                String pathChange = new String(tmp, "UTF-8");
-                return;
-            case Patterns.UPLOADFILE:
-                int pathDWFLen = command.readInt();
-                tmp = new byte[pathDWFLen];
-                command.readBytes(tmp);
-                String fileName = new String(tmp, "UTF-8");
-                System.out.println(command.readableBytes());
-                if (Files.exists(Paths.get(fileName))) {
-                    System.out.println("File already exists");
-                    while(command.isReadable()){
-                        command.readByte();
-                    }
-                    //отправлять сообщение клиенту чтобы он не продолжал слать файл
-                } else {
-                    try {
-                        Long fileSize = command.readLong();
-                        Long countPack = fileSize/256;
-                        if(fileSize % 256 != 0){countPack++;}
-                        System.out.println("count pack: "+countPack);
-                        Long readPack = 0l;
-                        System.out.println("filesize: "+fileSize);
-                        Files.createFile(Paths.get(fileName));
-                        tmp = new byte[256];
-                        while(readPack<countPack){
-                            //tmp = new byte[fileSize.intValue()];
-                            //command.readBytes(tmp);
-                            //Files.write(Paths.get(fileName),tmp);
-                            if(readPack+1==countPack){
-                                System.out.println("Readpack: "+readPack);
-                                System.out.println("last pack bytes: "+(fileSize-(readPack*256)));
-                                tmp = new byte[(int)(fileSize-(readPack*256))];
-                            }
-                            if(command.readableBytes()>=tmp.length){
+
+        public void checkCommand(ByteBuf command) throws Exception {
+            System.out.println("r1 "+command.readableBytes());
+            //executorService.execute(()->{
+            while(command.isReadable()) {
+                try {
+                    if (state == 0) {
+                        switch (command.readByte()) {
+                            case Patterns.CHANGEDIR:
+                                int pathCDLen = command.readInt();
+                                tmp = new byte[pathCDLen];
                                 command.readBytes(tmp);
-                            }
-                            readPack++;
-                            Files.write(Paths.get(fileName),tmp,StandardOpenOption.APPEND);
+                                String pathChange = new String(tmp, "UTF-8");
+                                return;
+                            case Patterns.UPLOADFILE:
+                                int pathDWFLen = command.readInt();
+                                tmp = new byte[pathDWFLen];
+                                command.readBytes(tmp);
+                                fileName = new String(tmp, "UTF-8");
+                                if (Files.exists(Paths.get(fileName))) {
+                                    System.out.println("File already exists");
+                                    while (command.isReadable()) {
+                                        command.readByte();
+                                    }
+                                    //отправлять сообщение клиенту чтобы он не продолжал слать файл
+                                } else {
+                                    fileSize = command.readLong();
+                                    countPack = fileSize / 256;
+                                    if (fileSize % 256 != 0) {
+                                        countPack++;
+                                    }
+                                    System.out.println("count pack: " + countPack);
+                                    Long readPack = 0l;
+                                    System.out.println("filesize: " + fileSize);
+                                    Files.createFile(Paths.get(fileName));
+                                    readBytes = 0l;
+                                }
+                                state = 1;
+                                return;
+                            case Patterns.DOWNLOADFILE:
+                                int pathUPFLen = command.readInt();
+                                tmp = new byte[pathUPFLen];
+                                command.readBytes(tmp);
+                                String pathDWF = new String(tmp, "UTF-8");
+                                //передача байтов самого файла
+                                return;
+                            case Patterns.DELETEFILE:
+                                int pathDFLen = command.readInt();
+                                tmp = new byte[pathDFLen];
+                                command.readBytes(tmp);
+                                String pathDF = new String(tmp, "UTF-8");
+                                Files.delete(Paths.get(pathDF));
+                                System.out.println("File: " + pathDF + " deleted ");
+                                return;
+                            case Patterns.GETFILELIST:
+                                int pathFLLen = command.readInt();
+                                tmp = new byte[pathFLLen];
+                                command.readBytes(tmp);
+                                String pathFL = new String(tmp, "UTF-8");
+                                Stream<Path> dir = Files.list(Paths.get(pathFL));
+                                Object[] dir2 = dir.toArray();
+                                for (Object obj : dir2) {
+                                    System.out.println(obj.toString().replace(root + "/", ""));
+                                }
+                                return;
+                            default:
+                                System.out.println("Wrong command");
+                                while (command.isReadable()) {
+                                    command.readByte();
+                                }
                         }
-                        System.out.println("Upload file: " + fileName+" done");
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+                    } else if (state == 1) {
+                        System.out.println("HiState1");
+                        System.out.println("r1 "+command.readableBytes());
+                        System.out.println(readBytes+"/"+fileSize);
+//                        tmp = new byte[256];
+//                        while (readPack < countPack) {
+//                            if (readPack + 1 == countPack) {
+//                                System.out.println("Readpack: " + readPack);
+//                                System.out.println("last pack bytes: " + (fileSize - (readPack * 256)));
+//                                tmp = new byte[(int) (fileSize - (readPack * 256))];
+//                            }
+//                            command.readBytes(tmp);
+//                            readPack++;
+//                            Files.write(Paths.get(fileName), tmp, StandardOpenOption.APPEND);
+//                        }
+//                        if (readPack == countPack) {
+//                            state = 0;
+//                            System.out.println("Upload file: " + fileName + " done");
+//                        }
 
-                return;
-            case Patterns.DOWNLOADFILE:
-                int pathUPFLen = command.readInt();
-                tmp = new byte[pathUPFLen];
-                command.readBytes(tmp);
-                String pathDWF = new String(tmp, "UTF-8");
-                //передача байтов самого файла
-                return;
-            case Patterns.DELETEFILE:
-                int pathDFLen = command.readInt();
-                tmp = new byte[pathDFLen];
-                command.readBytes(tmp);
-                String pathDF = new String(tmp, "UTF-8");
-                try {
-                    Files.delete(Paths.get(pathDF));
-                    System.out.println("File: " + pathDF +" deleted ");
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return;
-            case Patterns.GETFILELIST:
-                int pathFLLen = command.readInt();
-                tmp = new byte[pathFLLen];
-                command.readBytes(tmp);
-                String pathFL = new String(tmp, "UTF-8");
-                try {
-                Stream<Path> dir = Files.list(Paths.get(pathFL));
-                Object[] dir2 = dir.toArray();
-                    for (Object obj : dir2) {
-                        System.out.println(obj.toString().replace(root+"/",""));
+                         if (readBytes<fileSize){
+                            if(readBytes+command.readableBytes()<=fileSize){
+                                tmp = new byte[command.readableBytes()];
+                            }else{
+                                tmp = new byte[(int)(fileSize-readBytes)];
+                            }
+                             command.readBytes(tmp);
+                             readBytes += tmp.length;
+                            Files.write(Paths.get(fileName), tmp, StandardOpenOption.APPEND);
+                        }
+                        if (readBytes.equals(fileSize)){
+                            state = 0;
+                            System.out.println("Upload file: " + fileName + " done");
+                        }
                     }
-                } catch (IOException e) {
+
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
-                return;
-            default:
-                System.out.println("Wrong command");
+            }
+            //});
         }
     }
 }
